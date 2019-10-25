f5f1e69a99dd83e0ec514513c35e6902d9e6a5f2
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2019-10-24 23:52:05 -0500

    Added trailing slash for regex

--- Hide-Roomba-Bound-Posts/dist/StackHideRoombaBoundPosts.user.js
+++ Hide-Roomba-Bound-Posts/dist/StackHideRoombaBoundPosts.user.js
@@ -4,7 +4,7 @@
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
 // @version          1.0.1
-// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/tools
+// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/tools/
 // @grant            none
 // ==/UserScript==
 
