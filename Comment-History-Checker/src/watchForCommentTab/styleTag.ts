356768d7fa66931c85ef1a223e72de4041ea7669
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2019-10-27 15:35:20 -0500

    Switch from tsc to ts-loader

--- Comment-History-Checker/src/watchForCommentTab/styleTag.ts
+++ Comment-History-Checker/src/watchForCommentTab/styleTag.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
 // tslint:disable-next-line: no-implicit-dependencies
-import styleTextCss from 'raw-loader!./styleText.css';
+import styleTextCss from 'raw-loader!../../build/watchForCommentTab/styleText.css';
 export const styleTag = document.createElement('style');
 styleTag.textContent = styleTextCss as string;
