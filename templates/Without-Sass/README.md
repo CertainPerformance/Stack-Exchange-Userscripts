903739fc7eea304a2c3cdb7b22ec6467b7294849
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2019-11-04 00:35:26 -0600

    Fixed folder casing

---
+++ templates/Without-Sass/README.md
@@ -0,0 +1,1 @@
+# Userscript Name (&#8202;[install](https://github.com/CertainPerformance/Stack-Exchange-Userscripts/raw/master/Userscript-Name/dist/StackUserscriptName.user.js)&#8202;)
---
+++ templates/Without-Sass/src/userscript-metadata-block.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name             Stack USERSCRIPT NAME
+// @description      SOME DESCRIPTION
+// @author           CertainPerformance
+// @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
+// @version          1.0.0
+// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/ ... ...
+// @grant            none
+// ==/UserScript==
