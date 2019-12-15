a5560e685413d00601151a1225e3b081d858c1c7
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2019-12-14 21:49:15 -0600

    Made webpack config more generic

--- Vote-From-Review/webpack.config.js
+++ Vote-From-Review/webpack.config.js
@@ -1,29 +1,38 @@
 const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts']
-    },
-    output: {
-        filename: 'StackVoteFromReview.user.js',
-    },
-    devtool: 'none',
-    mode: 'development',
-    plugins: [
-        new webpack.BannerPlugin({
-            banner: fs.readFileSync('./src/userscript-metadata-block.js', 'utf-8'),
-            raw: true
-        })
-    ]
+module.exports = ({ liveDev = false } = {}) => {
+    const cwd = process.cwd();
+    const filename = `Stack${cwd.match(/[^/\\]+$/)[0].replace(/-/g, '')}${liveDev ? 'LiveDev' : ''}.user.js`;
+    return {
+        entry: './src/index.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts']
+        },
+        output: {
+            filename,
+            path: path.join(cwd, liveDev ? '' : '/dist')
+        },
+        devtool: 'none',
+        mode: 'development',
+        plugins: [
+            // Only prepend the metadata block to the generated .js when the dist-ready .js is created
+            // (With live dev, the local .js is being `@require`d by Tampermonkey, so the metadata block serves no purpose)
+            !liveDev && new webpack.BannerPlugin({
+                banner: fs.readFileSync('./src/userscript-metadata-block.js', 'utf-8'),
+                raw: true
+            })
+        ].filter(Boolean),
+        stats: liveDev ? 'minimal' : 'normal',
+    };
 };
