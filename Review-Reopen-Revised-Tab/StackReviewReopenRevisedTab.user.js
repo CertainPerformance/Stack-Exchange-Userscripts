f2752a89460f9f7b9a710409d4508d121c6c5da4
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-01-01 20:00:22 -0600

    Improved @includes for review queues

--- Review-Reopen-Revised-Tab/StackReviewReopenRevisedTab.user.js
+++ Review-Reopen-Revised-Tab/StackReviewReopenRevisedTab.user.js
@@ -3,15 +3,14 @@
 // @description      In Reopen Votes, automatically go to the "Revised" tab
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.0
-// @include          /^https://stackoverflow.com/review/reopen/\d+/
+// @version          1.0.1
+// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/reopen(?:/?$|/\d+)/
 // @grant            none
 // ==/UserScript==
-/* global $ */
 
 'use strict';
 
-$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
+window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
     if ((url.startsWith('/review/next-task/') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
         const revisedTabAnchor = document.querySelector('a.original-closed-post');
         if (revisedTabAnchor) {
