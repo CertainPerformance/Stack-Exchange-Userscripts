7ae2ac0140c0ba382644abdb28df67cdc847016d
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-01-13 13:08:12 -0600

    Keep textarea visible if post deleted while editing

--- Three-Columns/src/userscript-metadata-block.js
+++ Three-Columns/src/userscript-metadata-block.js
@@ -3,7 +3,7 @@
 // @description      When answering, editing, or asking, displays the question page, post textarea, and post preview in side-by-side columns
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.3.4
+// @version          1.3.5
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/(?:posts/\d+/edit|questions/(?:\d+|ask))/
 // @grant            none
 // ==/UserScript==
