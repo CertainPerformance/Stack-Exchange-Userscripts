a7080b02b0faf1539e629c16348342372995e7b0
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-02-27 17:09:09 -0600

    Fix comm. wiki selector change

--- Three-Columns/src/userscript-metadata-block.js
+++ Three-Columns/src/userscript-metadata-block.js
@@ -3,7 +3,7 @@
 // @description      When answering, editing, or asking, displays the question page, post textarea, and post preview in side-by-side columns
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.3.7
+// @version          1.3.8
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/(?:posts/\d+/edit|questions/(?:\d+|ask))/
 // @grant            none
 // ==/UserScript==
