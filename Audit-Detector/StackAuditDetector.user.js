3f2582d39579b7e085df839c6600ffba3266226a
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-03-25 17:21:28 -0500

    Fix SE class change

--- Audit-Detector/StackAuditDetector.user.js
+++ Audit-Detector/StackAuditDetector.user.js
@@ -3,8 +3,8 @@
 // @description      Detects audits in review queues
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.2
-// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/\w+\b(?!/stats|/history)/
+// @version          1.0.3
+// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/\w(?!.*/stats|.*/history)/
 // @grant            none
 // ==/UserScript==
 
@@ -12,7 +12,7 @@
 
 window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
     if ((url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
-        const reviewBar = document.querySelector('.review-bar');
+        const reviewBar = document.querySelector('.js-review-bar');
         reviewBar.style.backgroundColor = jqXHR.responseJSON.isAudit
             ? 'red'
             : 'yellow';
