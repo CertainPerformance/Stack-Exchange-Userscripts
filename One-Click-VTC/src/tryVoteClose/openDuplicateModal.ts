0b66b719841ab78bd608c5844fa4774e005b6f26
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-04-13 22:37:16 -0500

    Fix SE selector change
    Also

--- One-Click-VTC/src/tryVoteClose/openDuplicateModal.ts
+++ One-Click-VTC/src/tryVoteClose/openDuplicateModal.ts
@@ -1,7 +1,9 @@
 export const openDuplicateModal = () => {
     document.querySelector<HTMLAnchorElement>('.close-question-link')!.click();
     const handler = () => {
-        const duplicateRadio = document.querySelector<HTMLElement>('input[type="radio"][name="close-reason"][value="Duplicate"]');
+        // First ID selector below is new due to UI changes ~4/13/20: https://meta.stackoverflow.com/q/396592
+        // If it doesn't get reverted and makes it out of the testing phase, second selector can be removed
+        const duplicateRadio = document.querySelector<HTMLElement>('#closeReasonId-Duplicate, input[type="radio"][name="close-reason"][value="Duplicate"]');
         if (duplicateRadio) {
             // If there's an error, or user has already voted to close, duplicateRadio will not exist
             // That's fine - keep the newly opened modal or error box open, so user can see what the problem was
