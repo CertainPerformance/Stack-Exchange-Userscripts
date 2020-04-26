f3fac65be1494c8dca418538e4a3113e77cd2581
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-04-25 20:26:09 -0500

    Add run-at document-start

--- Audit-Detector/StackAuditDetector.user.js
+++ Audit-Detector/StackAuditDetector.user.js
@@ -3,18 +3,38 @@
 // @description      Detects audits in review queues
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.3
+// @version          1.0.4
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/\w(?!.*/stats|.*/history)/
+// @run-at           document-start
 // @grant            none
 // ==/UserScript==
 
+// The @run-at document-start will ensure that the userscript runs in time, before SE has a chance to respond with a new task
+// Without it, given certain setups, the first task loaded into the page will infrequently not be detected by the below code
+// Experimental Inject Mode: Instant isn't required
+
 'use strict';
 
-window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
-    if ((url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
-        const reviewBar = document.querySelector('.js-review-bar');
-        reviewBar.style.backgroundColor = jqXHR.responseJSON.isAudit
-            ? 'red'
-            : 'yellow';
-    }
-});
+const main = () => {
+    window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
+        if ((url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
+            const reviewBar = document.querySelector('.js-review-bar');
+            reviewBar.style.backgroundColor = jqXHR.responseJSON.isAudit
+                ? 'red'
+                : 'yellow';
+        }
+    });
+};
+if (window.$) {
+    main();
+} else {
+    Object.defineProperty(window, '$', {
+        configurable: true,
+        set(newVal) {
+            // Remove this setter:
+            delete window.$;
+            window.$ = newVal;
+            main();
+        },
+    });
+}
