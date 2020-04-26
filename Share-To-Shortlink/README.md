43d014121d12dfaf94acb95ddfa7f65f57402492
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-04-25 20:30:04 -0500

    Add Share To Shortlink

---
+++ Share-To-Shortlink/README.md
@@ -0,0 +1,1 @@
+Replaces "Share" link with a shortlink not linked to your account
---
+++ Share-To-Shortlink/StackShareToShortlink.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name             Stack Share to Shortlink
+// @description      Replaces "Share" link with a shortlink not linked to your account
+// @author           CertainPerformance
+// @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
+// @version          1.0.0
+// @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/questions/\d/
+// @grant            none
+// ==/UserScript==
+
+'use strict';
+
+for (const a of document.querySelectorAll('.js-share-link')) {
+    const href = a.href.match(/\D+\d+/)[0];
+    a.outerHTML = `<a href="${href}" title="short permalink to this question">shortlink</a>`;
+}
