e8dcd38b860e4d8a49182c073ab03be02dc66245
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-04-28 15:53:14 -0500

    Be tolerant of TS errors with LiveDev versions

--- Three-Columns/webpack.config.js
+++ Three-Columns/webpack.config.js
@@ -7,13 +7,13 @@ const webpack = require('webpack');
 module.exports = ({ liveDev = false } = {}) => {
     const cwd = process.cwd();
     const filename = `Stack${cwd.match(/[^/\\]+$/)[0].replace(/-/g, '')}${liveDev ? 'LiveDev' : ''}.user.js`;
-    return {
+    const webpackConfigObj = {
         entry: './src/index.ts',
         module: {
             rules: [
                 {
                     test: /\.ts$/,
-                    use: 'ts-loader',
+                    loader: 'ts-loader',
                     exclude: /node_modules/,
                 },
             ],
@@ -25,25 +25,26 @@ module.exports = ({ liveDev = false } = {}) => {
             filename,
             path: path.join(cwd, liveDev ? '' : '/dist'),
         },
-        devtool: 'none', // Keep the generated .js readable
+        devtool: 'none', // Keep the generated .js readable; no eval shenanigans
         mode: 'development', // Never minify. Completely different from liveDev
         plugins: [
-            // Only prepend the metadata block to the generated .js when the dist-ready .js is created
-            // (With live dev, the local .js is being `@require`d by Tampermonkey, so the metadata block serves no purpose)
-            !liveDev && new webpack.BannerPlugin({
-                banner: fs.readFileSync('./src/userscript-metadata-block.js', 'utf-8'),
+            new webpack.BannerPlugin({
+                banner: (liveDev ? '// LIVE DEV\n\n' : '') + fs.readFileSync('./src/userscript-metadata-block.js', 'utf-8'),
                 raw: true,
             }),
-            !liveDev && {
-                apply: (compiler) => {
-                    compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
-                        const distTextContent = fs.readFileSync('./dist/StackThreeColumns.user.js', 'utf-8');
-                        // Only change the metadata block:
-                        fs.writeFileSync('./dist/StackThreeColumnsAdjustable.user.js', distTextContent.replace('Three Columns', 'Three Columns Adjustable'));
-                    });
-                },
-            },
-        ].filter(Boolean),
+        ],
         stats: liveDev ? 'minimal' : 'normal',
     };
+    if (liveDev) {
+        webpackConfigObj.module.rules[0].options = {
+            // When debugging/experimenting with live dev version, don't require type accuracy before compilation
+            transpileOnly: true,
+            compilerOptions: {
+                outDir: './build',
+                module: 'commonjs',
+                target: 'es2017',
+            },
+        };
+    }
+    return webpackConfigObj;
 };
