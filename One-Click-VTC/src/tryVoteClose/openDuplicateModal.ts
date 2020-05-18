b993359fe64e2a450aa58d5bad077f7273fa1b25
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-05-18 18:18:00 -0500

    Remove old unused selector

--- One-Click-VTC/src/tryVoteClose/openDuplicateModal.ts
+++ One-Click-VTC/src/tryVoteClose/openDuplicateModal.ts
@@ -4,9 +4,7 @@ export const openDuplicateModal = () => {
         if (!ajaxOptions.url || !/\/flags\/questions\/\d+\/close\/popup/.test(ajaxOptions.url)) {
             return;
         }
-        // First ID selector below is new due to UI changes ~4/13/20: https://meta.stackoverflow.com/q/396592
-        // If it doesn't get reverted and makes it out of the testing phase, second selector can be removed
-        const duplicateRadio = document.querySelector<HTMLElement>('#closeReasonId-Duplicate, input[type="radio"][name="close-reason"][value="Duplicate"]');
+        const duplicateRadio = document.querySelector<HTMLElement>('#closeReasonId-Duplicate');
         if (duplicateRadio) {
             // If there's an error, or user has already voted to close, duplicateRadio will not exist
             // That's fine - keep the newly opened modal or error box open, so user can see what the problem was
