fb72a24830617622c94e8ac9457f4ff8a8fe28a8
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-08-27 13:34:40 -0500

    Reduce flickering on pageload

--- Compact-Line-Height/StackCompactLineHeight.user.js
+++ Compact-Line-Height/StackCompactLineHeight.user.js
@@ -1,17 +1,24 @@
 // ==UserScript==
 // @name             Stack Compact Line Height
-// @description      Reduces the space between lines to what it used to be
+// @description      Reduces the space between lines back to what it used to be
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.0
+// @version          1.0.1
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)//
+// @run-at           document-start
 // @grant            none
 // ==/UserScript==
 
+// SEMI-IMPORTANT: If you wish to avoid flickering on pageload, this script must run before the page content loads
+// If using Tampermonkey, you may need to enable instant script injection via:
+// Settings -> Experimental -> Inject Mode -> Instant
+
 'use strict';
 
-document.body.appendChild(document.createElement('style')).textContent = `
+(document.head || document.documentElement).appendChild(document.createElement('style')).textContent = `
 .s-prose {
-    line-height: 1.3;
+    /* Feel free to adjust the number below as desired */
+    /* 1.3 was the old line-height, 1.6 is the new line-height */
+    line-height: 1.3 !important;
 }
 `;
