c82ca146640e48ef2741a10549b6640594ac2ac7
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-08-27 12:49:25 -0500

    Fix selector change

--- Review-Reopen-Revised-Tab/StackReviewReopenRevisedTab.user.js
+++ Review-Reopen-Revised-Tab/StackReviewReopenRevisedTab.user.js
@@ -3,7 +3,7 @@
 // @description      In Reopen Votes, automatically go to the "Revised" tab
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.2
+// @version          1.0.3
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/reopen(?!/stats|/history)/
 // @grant            none
 // ==/UserScript==
@@ -12,7 +12,7 @@
 
 window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
     if ((url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
-        const revisedTabAnchor = document.querySelector('a.original-closed-post');
+        const revisedTabAnchor = document.querySelector('a[id^="reviewable-post-"]');
         if (revisedTabAnchor) {
             revisedTabAnchor.click();
         }
