f74472c5eae4f8f892e147f20ba2ceb260c0b0cb
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2020-08-28 09:05:45 -0500

    Add spacing between paragraphs

--- Compact-Line-Height/StackCompactLineHeight.user.js
+++ Compact-Line-Height/StackCompactLineHeight.user.js
@@ -3,7 +3,7 @@
 // @description      Reduces the space between lines back to what it used to be
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.1.0
+// @version          1.2.0
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)//
 // @run-at           document-start
 // @grant            none
@@ -15,18 +15,24 @@
 
 'use strict';
 
+// Feel free to adjust the numbers below as desired
+
 (document.head || document.documentElement).appendChild(document.createElement('style')).textContent = `
 /* Spacing for normal text: */
 .s-prose {
-    /* Feel free to adjust the number below as desired */
     /* 1.3 was the old line-height, 1.5 is the new line-height */
     line-height: 1.3 !important;
 }
 
 /* Spacing for code blocks: */
 .s-prose pre:not(.s-code-block) {
-    /* Feel free to adjust the number below as desired */
     /* 1.3 was the old line-height, 1.30769231; is the new line-height */
     line-height: 1.3 !important;
+}
+
+/* Spacing between paragraphs: */
+.s-prose p {
+    /* 15px (equal to 1em) was the old margin-bottom, 1.4em (equal to 21px); is the new margin-bottom */
+    margin-bottom: 15px !important;
 }
 `;
