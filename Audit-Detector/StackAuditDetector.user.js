3d5a81e6302c5e41e5f7b3c01e4f0da9af50178f
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2021-02-27 09:51:22 -0600

    Fix for review layout change

--- Audit-Detector/StackAuditDetector.user.js
+++ Audit-Detector/StackAuditDetector.user.js
@@ -3,7 +3,7 @@
 // @description      Detects audits in review queues
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.5
+// @version          1.0.6
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/\w(?!.*/stats|.*/history)/
 // @run-at           document-start
 // @grant            none
@@ -18,7 +18,7 @@
 const main = () => {
     window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
         if ((url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
-            const reviewBar = document.querySelector('.js-review-bar');
+            const reviewBar = document.querySelector('.mainbar-full > .grid > .grid--cell');
             reviewBar.style.backgroundColor = jqXHR.responseJSON.isAudit
                 ? 'var(--red-200)' // CSS variables differ depending on whether in light or dark mode
                 : 'var(--blue-200)'; // Using these ensures reasonable contrast regardless
