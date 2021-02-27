c6686c64f60e1c7ee1c437704fb303b1ac84ca31
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2021-02-27 09:57:43 -0600

    Fix for review layout change

--- Review-Reopen-Revised-Tab/StackReviewReopenRevisedTab.user.js
+++ Review-Reopen-Revised-Tab/StackReviewReopenRevisedTab.user.js
@@ -3,7 +3,7 @@
 // @description      In Reopen Votes, automatically go to the "Revised" tab
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.3
+// @version          1.0.4
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/review/reopen(?!/stats|/history)/
 // @grant            none
 // ==/UserScript==
@@ -12,9 +12,12 @@
 
 window.$(document).on('ajaxComplete', (event, jqXHR, { url }) => {
     if ((url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) && jqXHR.responseJSON.reviewTaskId) {
-        const revisedTabAnchor = document.querySelector('a[id^="reviewable-post-"]');
-        if (revisedTabAnchor) {
-            revisedTabAnchor.click();
-        }
+        Promise.resolve().then(() => {
+            const revisedTabAnchor = document.querySelector('.js-review-content button');
+            console.log(revisedTabAnchor);
+            if (revisedTabAnchor) {
+                revisedTabAnchor.click();
+            }
+        });
     }
 });
