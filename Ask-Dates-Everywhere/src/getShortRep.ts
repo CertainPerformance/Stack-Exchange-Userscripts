f96c503b0a6d9fe6d97e5b20168898a8bbcfde90
CertainPerformance <37674520+CertainPerformance@users.noreply.github.com>
2021-10-22 18:16:34 -0500

    Improve rep display logic

--- Ask-Dates-Everywhere/src/getShortRep.ts
+++ Ask-Dates-Everywhere/src/getShortRep.ts
@@ -5,8 +5,10 @@ import { addThousandsSeparators } from './addThousandsSeparators';
  * 900 -> 900
  * 1111 -> 1,111
  * 9999 -> 9,999
- * 10000 -> 10k
- * 11111 -> 11k
+ * 10_000 -> 10k
+ * 10_100 -> 10.1k
+ * 99_900 -> 99.9k
+ * 100_100 -> 100k
  * 234_567 -> 235k
  * 1_000_000 -> 1000k
  */
@@ -16,6 +18,12 @@ export const getShortRep = (reputation: number) => {
     }
     if (reputation < 10_000) {
         return addThousandsSeparators(reputation);
+    }
+    if (reputation < 100_000) {
+        const hundreds = Math.round(reputation / 100);
+        return hundreds % 10 === 0
+            ? `${hundreds / 10}k`
+            : String(hundreds).replace(/.$/, '.$&') + 'k';
     }
     const thousands = Math.round(reputation / 1000);
     return `${thousands}k`;
