d26effa087bb1ccfc46a4bfcbd4202533603721d
cherryblossom000 <31467609+cherryblossom000@users.noreply.github.com>
2021-11-02 16:11:00 +1100

    update version

--- Highlight-Unfolded-Comments/StackHighlightUnfoldedComments.user.js
+++ Highlight-Unfolded-Comments/StackHighlightUnfoldedComments.user.js
@@ -3,7 +3,7 @@
 // @description      Keeps newly-unfolded comments highlighted, to easily distinguish them from higher-scoring comments you've already read
 // @author           CertainPerformance
 // @namespace        https://github.com/CertainPerformance/Stack-Exchange-Userscripts
-// @version          1.0.6
+// @version          1.0.7
 // @include          /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/(?:questions/\d|review/\w(?!.*/stats|.*/history))/
 // @grant            none
 // ==/UserScript==
